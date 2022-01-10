0d84a71e4071bfcbd19d0cc7655354c59fa202aa
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-01-10 17:58:34 +0100

    Zmiany w Lab13Zad4

--- Zad4/features/steps/planetAge.py
+++ Zad4/features/steps/planetAge.py
@@ -3,13 +3,34 @@ from behave import *
 use_step_matcher("parse")
 
 
-@given('Input seconds and planet')
+@given('Input seconds and planet (string)')
 def step_impl(context):
     try:
         context.seconds = int(context.text.split(",")[0])
     except ValueError:
         context.seconds = context.text.split(",")[0]
     context.method = context.text.split(",")[1]
+
+
+@given('Input seconds and planet (and)')
+def step_impl(context):
+    context.seconds = None
+    context.method = None
+
+
+@given('seconds {seconds:d}')
+def step_impl(context, seconds):
+    context.seconds = seconds
+
+
+@given('seconds {seconds}')
+def step_impl(context, seconds):
+    context.seconds = seconds
+
+
+@given('planet {method}')
+def step_impl(context, method):
+    context.method = method
 
 
 @when('calculate the planet age')
